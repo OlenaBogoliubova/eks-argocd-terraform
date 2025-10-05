4b5b6046faebfc790fbfd2d4a13747e764731644
OlenaBogoliubova <143827030+OlenaBogoliubova@users.noreply.github.com>
2025-10-05 12:20:44 +0200

    Update main.tf
    map_public_ip_on_launch = true

--- terraform/vpc/main.tf
+++ terraform/vpc/main.tf
@@ -11,6 +11,7 @@ module "vpc" {
     enable_nat_gateway = false
     enable_dns_hostnames = true
     enable_dns_support = true
+    map_public_ip_on_launch = true
 
     tags = {
         Project = "eks-argocd"
